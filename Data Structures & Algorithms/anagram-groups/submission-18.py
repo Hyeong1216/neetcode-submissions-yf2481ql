b6d21fbb17a985b9cddcdc18335311f575c62f9a
Hyeong1216 <hyeongbusinessacct@gmail.com>
2026-04-15 07:27:06 -0700

    Add: anagram-groups - submission-18

---
+++ Data Structures & Algorithms/anagram-groups/submission-18.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # 1. sorting
+        # res = defaultdict(list)
+        # for s in strs:
+        #     sortedS = ''.join(sorted(s))
+        #     res[sortedS].append(s)
+        # return list(res.values())
+
+        # 2. Hash Table O(m*n)
+        ans = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c)-ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return list(ans.values())
+
+        
