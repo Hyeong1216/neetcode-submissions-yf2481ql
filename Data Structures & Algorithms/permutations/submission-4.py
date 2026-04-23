10a0bc61708d69becb3f9ed598f2aedff3d45b83
Hyeong1216 <hyeongbusinessacct@gmail.com>
2026-04-23 04:43:11 -0700

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        visited = set()
+
+        def bt(visited, subset):
+            if len(visited) == n:
+                res.append(subset[:])
+                return
+            for i in range(n):
+                if i not in visited:
+                    subset.append(nums[i])
+                    visited.add(i)
+                    bt(visited, subset)
+                    subset.pop()
+                    visited.remove(i)
+
+
+
+
+
+        bt(visited, [])
+
+        return res
