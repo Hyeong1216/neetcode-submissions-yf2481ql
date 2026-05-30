5e6f8c61e70821fe630de91557cef87360c711f7
Hyeong1216 <hyeongbusinessacct@gmail.com>
2026-05-30 06:25:35 -0700

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Questions
+        # 1. there are at least one element in the nums?
+        # 2. can all element be negative?
+        # 3. Are there any constraints on the array size?
+
+        # Kadane's algorithm
+        curr_sum = nums[0]
+        max_sum = nums[0]
+
+        for num in nums[1:]:
+            curr_sum = max(num, curr_sum+num)
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
