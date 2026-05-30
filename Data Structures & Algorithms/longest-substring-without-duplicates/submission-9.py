286f5a4507e8aab069c631a9c240c827d979881d
Hyeong1216 <hyeongbusinessacct@gmail.com>
2026-05-30 06:47:49 -0700

    Add: longest-substring-without-duplicates - submission-9

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-9.py
@@ -0,0 +1,49 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # Questions
+        # 1. can there be numbers or special characters?
+        # 2. Can the string be empty?
+        # 3. Is it case sensitive?
+        # 4. Can there be spaces?
+
+        # answer
+        l, res = 0, 0
+        charSet = set()
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(r-l+1, res)
+            r += 1
+        
+
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # l, res = 0, 0
+        # charSet = set()
+
+        # for r in range(len(s)):
+        #     while s[r] in charSet:
+        #         charSet.remove(s[l])
+        #         l += 1
+        #     charSet.add(s[r])
+        #     res = max(res, r - l + 1)
+        #     r += 1
+
+        # return res
