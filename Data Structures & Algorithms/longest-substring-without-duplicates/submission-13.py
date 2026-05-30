ff9226a656aafed6420cac24c5024c716ef39483
Hyeong1216 <hyeongbusinessacct@gmail.com>
2026-05-30 06:50:52 -0700

    Add: longest-substring-without-duplicates - submission-13

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-13.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # Questions
+        # 1. can there be numbers or special characters?
+        # 2. Can the string be empty?
+        # 3. Is it case sensitive?
+        # 4. Can there be spaces?
+
+        # answer
+        l, res = 0, 0
+        charSet = set()
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(r-l+1, res)
+            
+        return res
+
+        # O(n)
