51d6c6cc545af5c5d4c957b0fcc4a2b66948ad5e
Hyeong1216 <hyeongbusinessacct@gmail.com>
2026-08-07 00:45:53 -0700

    Add: two-integer-sum - submission-28

---
+++ Data Structures & Algorithms/two-integer-sum/submission-28.py
@@ -0,0 +1,70 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # hashmap
+        Map = defaultdict(int)
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in Map:
+                return [Map.get(diff), i]
+            Map[nums[i]] = i
+        return []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         # Questions
+#         # 1. Can the array have duplicate values?
+#         # 2. Is there always exacrlt one solution?
+#         # 3. Can I use the same element twice?
+#         # 4. Should I return indices or values?
+#         # 5. is the array sorted?
+
+#         # Solutions
+#         #1. Brute force O(n^2)
+#         # for i in range(len(nums)):
+#         #     for j in range(i+1, len(nums)):
+#         #         temp = nums[i] + nums[j]
+#         #         if temp == target:
+#         #             return [i, j]
+
+#         # 2. Hashmap one pass O(n)
+#         Map = {}
+#         for i in range(len(nums)):
+#             diff = target - nums[i]
+#             if diff in Map:
+#                 return [Map.get(diff), i]
+#             Map[nums[i]] = i
+        
+
+
+#         # Map = {}
+#         # for i in range(len(nums)):
+#         #     diff = target - nums[i]
+#         #     if diff in Map:
+#         #         return [Map.get(diff), i]
+#         #     Map[nums[i]] = i
+
+#         # Time: O(n), Space: O(n) — hashmap one pass
+
