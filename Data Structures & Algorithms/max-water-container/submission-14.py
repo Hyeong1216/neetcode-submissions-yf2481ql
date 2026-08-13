c6b9755b0c704c85ec8af13160b35846e79eee80
Hyeong1216 <hyeongbusinessacct@gmail.com>
2026-08-13 16:24:02 -0700

    Add: max-water-container - submission-14

---
+++ Data Structures & Algorithms/max-water-container/submission-14.py
@@ -0,0 +1,130 @@
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        res = float("-inf")
+        l, r = 0, len(heights) - 1
+
+        while l < r:
+            curr_area = min(heights[l], heights[r]) * (r-l)
+            res = max(res, curr_area)
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1
+
+
+
+        return res
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def maxArea(self, heights: List[int]) -> int:
+#         res = float("-inf")
+#         l, r = 0, len(heights)-1
+#         # print(min(heights[1], heights[7]) * (7 - 1))
+
+#         while l < r:
+#             curr_area = min(heights[l], heights[r]) * (r - l)
+#             # print(curr_area)
+#             res = max(curr_area, res)
+#             if heights[l] >= heights[r]:
+#                 r -= 1
+#             else:
+#                 l += 1
+
+
+#         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def maxArea(self, heights: List[int]) -> int:
+#         # Questions 
+#         # 1. Can the array be empty or have less than 2 elements?
+#         # 2. Can heights ne negative?
+
+#         # solution
+#         l, r = 0, len(heights) - 1
+#         maxArea = 0
+#         while l < r:
+#             curr_area = min(heights[l], heights[r]) * (r-l)
+#             maxArea = max(maxArea, curr_area)
+#             if heights[l] < heights[r]:
+#                 l += 1
+#             else:
+#                 r -= 1
+
+
+#         return maxArea
+
+#         # BigO -> O(N)
+
+
+
+
+
+
+
+
+
+
+
+
+#         l, r = 0, len(heights) - 1
+#         area = 0
+
+#         while l < r:
+#             currArea = (r-l) * min(heights[l], heights[r])
+#             area = max(area, currArea)
+#             if heights[l] <= heights[r]:
+#                 l += 1
+#             else:
+#                 r -= 1
+
+#         return area
