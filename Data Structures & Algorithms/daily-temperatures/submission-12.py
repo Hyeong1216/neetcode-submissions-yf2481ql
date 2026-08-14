310883d7810f569ae564c38e704c0b5f0908dd82
Hyeong1216 <hyeongbusinessacct@gmail.com>
2026-08-13 20:49:13 -0700

    Add: daily-temperatures - submission-12

---
+++ Data Structures & Algorithms/daily-temperatures/submission-12.py
@@ -0,0 +1,129 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        temper = temperatures
+        stack = []
+        res = [0] * len(temper)
+
+        for i in range(len(temper)):
+            while stack and temper[stack[-1]] < temper[i]:
+                prev_index = stack.pop()
+                res[prev_index] = i - prev_index
+            stack.append(i)
+
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+#         stack = []
+#         res = [0] * len(temperatures)
+
+#         for i in range(len(temperatures)):
+#             curr_temp = temperatures[i]
+#             while stack and temperatures[stack[-1]] < curr_temp:
+#                 prev_day_index = stack.pop()
+#                 res[prev_day_index] = i - prev_day_index
+#             stack.append(i)
+
+#         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+#         res = [0] * len(temperatures)
+#         stack = []
+#         for i, temp in enumerate(temperatures):
+#             while stack and temperatures[stack[-1]] < temperatures[i]:
+#                 prev_index = stack.pop()
+#                 res[prev_index] = i - prev_index
+#             stack.append(i)
+
+
+#         return res
+#         #----------------
+#         # res = [0] * len(temperatures)
+#         # stack = []
+
+#         # for i, temp in enumerate(temperatures):
+#         #     while stack and temperatures[stack[-1]] < temperatures[i]:
+#         #         prev_index = stack.pop()
+#         #         res[prev_index] = i - prev_index
+
+#         #     stack.append(i)
+#         # return res
+        
+#         #----------------
+#         # brute force
+#         # res = []
+#         # for i in range(len(temperatures)):
+#         #     count = 0
+#         #     for j in range(i+1, len(temperatures)):
+#         #         if temperatures[i] < temperatures[j]:
+#         #             count = j - i
+#         #             break
+#         #     res.append(count)
+
+#         # return res
