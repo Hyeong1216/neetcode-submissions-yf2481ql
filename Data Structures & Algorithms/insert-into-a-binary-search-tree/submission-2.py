a4dc600c38037fcb153941f6460b8b076e2cfae7
Hyeong1216 <hyeongbusinessacct@gmail.com>
2026-08-28 08:42:46 -0700

    Add: insert-into-a-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/insert-into-a-binary-search-tree/submission-2.py
@@ -0,0 +1,70 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # Iterative
+        if not root:
+            return TreeNode(val)
+        cur = root
+        while True:
+            if cur.val < val:
+                if not cur.right:
+                    cur.right = TreeNode(val)
+                    return root
+                cur = cur.right
+            else:
+                if not cur.left:
+                    cur.left = TreeNode(val)
+                    return root
+                cur= cur.left
+
+
+
+        # Recursion
+
+        
+
+
+
+
+
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+#         # Iterative
+#         if not root:
+#             return TreeNode(val)
+        
+#         cur = root
+#         while True:
+#             if val > cur.val:
+#                 if not cur.right:
+#                     cur.right = TreeNode(val)
+#                     return root
+#                 cur = cur.right
+            
+#             else:
+#                 if not cur.left:
+#                     cur.left = TreeNode(val)
+#                     return root
+#                 cur = cur.left
+
+
+#         # Recursion
+#         # if not root:
+#         #     return TreeNode(val)
+#         # if root.val < val:
+#         #     root.right = self.insertIntoBST(root.right, val)
+#         # else:
+#         #     root.left = self.insertIntoBST(root.left, val)
+#         # return root
+        
